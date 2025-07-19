91d69f1ab6d701cbc9b3fea6614d4c0c9be74066
fatheddine-bicane <fatheddine04@gmail.com>
2025-07-19 14:53:28 +0100

    init: main class Lox and man function

---
+++ jlox/Lox.java
@@ -0,0 +1,25 @@
+package jlox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+public class Lox {
+
+	public static void main(String[] args) throws IOException {
+		// if no argument are passed to the interpreter
+		if (1 < args.length) {
+			System.out.println("Usage: jlox [script]");
+			System.exit(64);
+		} else if (args.length == 1) {
+			runFile(args[0]);
+		} else {
+			runPrompt();
+		}
+	}
+}
