01d5afdbe835eb9a3158cfb88251b5114f460056
fatheddine-bicane <fatheddine04@gmail.com>
2025-09-27 17:01:30 +0100

    fix: indentation levels in the generated Expr file

--- jlox/lox/Lox.java
+++ jlox/lox/Lox.java
@@ -36,7 +36,7 @@ public class Lox {
 		InputStreamReader input = new InputStreamReader(System.in);
 		BufferedReader reader = new BufferedReader(input);
 
-		for (;;) {
+		while (true) {
 			System.out.print("> ");
 			String line = reader.readLine();
 			if (null == line)
