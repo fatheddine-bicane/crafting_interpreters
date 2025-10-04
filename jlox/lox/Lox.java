43d0bcd10dae2bf8c9097ed5dee35e52aa5f3a41
fatheddine-bicane <fatheddine04@gmail.com>
2025-10-04 22:09:45 +0100

    feat: interprete and run the tree structure parsed

--- jlox/lox/Lox.java
+++ jlox/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+	private static final Interpreter INTERPRETER = new Interpreter();
 	static boolean hadError = false;
+	static boolean hadRuntimeError = false;
 
 	public static void main(String[] args) throws IOException {
 		// if no argument are passed to the interpreter
@@ -27,8 +29,8 @@ public class Lox {
 		byte[] bytes = Files.readAllBytes(Paths.get(path));
 
 		run(new String(bytes, Charset.defaultCharset()));
-		if (true == hadError)
-			System.exit(65);
+		if (hadError) System.exit(65);
+		else if (hadRuntimeError) System.exit(70);
 	}
 
 	private static void runPrompt() throws IOException {
@@ -54,7 +56,11 @@ public class Lox {
 		// stop if there was a syntax error
 		if (hadError) return;
 
-		System.out.println(new AstPrinter().print(expression));
+		// interprete expression
+		INTERPRETER.interpret(expression);
+
+		// print expression
+		// System.out.println(new AstPrinter().print(expression));
 	}
 
 	static void error(int line, String message) {
@@ -72,5 +78,11 @@ public class Lox {
 		} else {
 			report(token.line, " at '" + token.lexeme + "'", message);
 		}
+	}
+
+	static void runtimeError(RuntimeError error) {
+		System.err.println(error.getMessage() +
+				"\n[line " + error.token.line + "]");
+		hadRuntimeError = true;
 	}
 }
