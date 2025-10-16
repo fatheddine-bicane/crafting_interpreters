e994943b42d4832018c3c30671e2669d5eed6000
fatheddine-bicane <fatheddine04@gmail.com>
2025-10-16 17:01:53 +0100

    feat: for and while lops whith lgical operators (or, and)

--- jlox/lox/Parser.java
+++ jlox/lox/Parser.java
@@ -2,11 +2,14 @@ package jlox.lox;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import static jlox.lox.TokenType.*;
 
 // NOTE: these down are rules that define how an expression in Lox is defined
 // expression     → assignment ;
-// assignment     → IDENTIFIER "=" assignment | equality ;
+// assignment     → IDENTIFIER "=" assignment | logic_or ;
+// logic_or       → logic_and ( "or" logic_and )* ;
+// logic_and      → equality ( "and" equality )* ;
 // equality       → comparison ( ( "!=" | "==" ) comparison )* ;
 // comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
 // term           → factor ( ( "-" | "+" ) factor )* ;
@@ -18,13 +21,22 @@ import static jlox.lox.TokenType.*;
 // program        → statement* EOF ;
 // declaration    → varDecl
 //                | statement ;
-// statement      → exprStmt
-//                | printStmt
-//                | block ;
-// block          → "{" declaration* "}" ;
 // varDecl        → "var" IDENTIFIER ( "=" expression )? ";" ;
+// statement      → exprStmt
+//                | forStmt
+//                | ifStmt
+//                | printStmt
+//                | whileStmt
+//                | block ;
 // exprStmt       → expression ";" ;
+// forStmt        → "for" "(" ( varDecl | exprStmt | ";" )
+//                  expression? ";"
+//                  expression? ")" statement ;
+// ifStmt         → "if" "(" expression ")" statement
+//                ( "else" statement )? ;
 // printStmt      → "print" expression ";" ;
+// whileStmt      → "while" "(" expression ")" statement ;
+// block          → "{" declaration* "}" ;
 
 class Parser {
 	private static class ParseError extends RuntimeException {}
@@ -68,9 +80,58 @@ class Parser {
 	}
 
 	private Stmt statement() {
-		if (match(PRINT)) return printStatement();
+		if (match(FOR)) return forStatement();
+		else if (match(IF)) return ifStatement();
+		else if (match(PRINT)) return printStatement();
+		else if (match(WHILE)) return whileStatement();
 		else if (match(LEFT_BRACE)) return new Stmt.Block(block());
 		return expressionStatement();
+	}
+
+	private Stmt forStatement() {
+		consume(LEFT_PAREN, "Expect '(' after 'for'.");
+		Stmt initializer;
+		if (match(SEMICOLON)) {
+			initializer = null;
+		} else if (match(VAR)) {
+			initializer = varDeclaration();
+		} else {
+			initializer = expressionStatement();
+		}
+		Expr condition = null;
+		if (!check(SEMICOLON)) {
+			condition = expression();
+		}
+		consume(SEMICOLON, "Expect ';' after loop condition.");
+		Expr increment = null;
+		if (!check(RIGHT_PAREN)) {
+			increment = expression();
+		}
+		consume(RIGHT_PAREN, "Expect ')' after for clauses.");
+		Stmt body = statement();
+		if (increment != null) {
+			body = new Stmt.Block(Arrays.asList(body, new Stmt.Expression(increment)));
+		}
+		if (condition == null) condition = new Expr.Literal(true);
+		body = new Stmt.While(condition, body);
+		if (initializer != null) {
+			body = new Stmt.Block(Arrays.asList(initializer, body));
+		}
+		return body;
+	}
+
+	private Stmt ifStatement() {
+		consume(LEFT_PAREN, "Expect '(' after 'if'.");
+		Expr condition = expression();
+		consume(RIGHT_PAREN, "Expect ')' after if condition."); 
+
+		Stmt thenBranch = statement();
+		Stmt elseBranch = null;
+		if (match(ELSE)) {
+			elseBranch = statement();
+		}
+
+		return new Stmt.If(condition, thenBranch, elseBranch);
 	}
 
 	// printStmt      → "print" expression ";" ;
@@ -90,6 +151,15 @@ class Parser {
 		}
 		consume(SEMICOLON, "Expect ';' after variable declaration.");
 		return new Stmt.Var(name, initializer);
+	}
+
+	// whileStmt      → "while" "(" expression ")" statement ;
+	private Stmt whileStatement() {
+		consume(LEFT_PAREN, "Expect '(' after 'while'.");
+		Expr condition = expression();
+		consume(RIGHT_PAREN, "Expect ')' after condition.");
+		Stmt body = statement();
+		return new Stmt.While(condition, body);
 	}
 
 	// exprStmt       → expression ";" ;
@@ -112,7 +182,7 @@ class Parser {
 
 	// assignment     → IDENTIFIER "=" assignment | equality ;
 	private Expr assignment() {
-		Expr expr = equality();
+		Expr expr = or();
 
 		if (match(EQUAL)) {
 			Token equals = previous();
@@ -122,6 +192,30 @@ class Parser {
 				return new Expr.Assign(name, value);
 			}
 			error(equals, "Invalid assignment target."); 
+		}
+		return expr;
+	}
+
+	// logic_or       → logic_and ( "or" logic_and )* ;
+	private Expr or() {
+		Expr expr = and();
+
+		while (match(OR)) {
+			Token operator = previous();
+			Expr right = and();
+			expr = new Expr.Logical(expr, operator, right);
+		}
+		return expr;
+	}
+
+	// logic_and      → equality ( "and" equality )* ;
+	private Expr and() {
+		Expr expr = equality();
+
+		while (match(AND)) {
+			Token operator = previous();
+			Expr right = equality();
+			expr = new Expr.Logical(expr, operator, right);
 		}
 		return expr;
 	}
