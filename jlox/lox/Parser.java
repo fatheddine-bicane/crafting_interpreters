059816a73d21c43d23c018a153b08ed44db83433
fatheddine-bicane <fatheddine04@gmail.com>
2025-10-17 22:32:41 +0100

    feat: functions call and return statements

--- jlox/lox/Parser.java
+++ jlox/lox/Parser.java
@@ -14,18 +14,24 @@ import static jlox.lox.TokenType.*;
 // comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
 // term           → factor ( ( "-" | "+" ) factor )* ;
 // factor         → unary ( ( "/" | "*" ) unary )* ;
-// unary          → ( "!" | "-" ) unary
-//                | primary ;
+// unary          → ( "!" | "-" ) unary | call ;
+// call           → primary ( "(" arguments? ")" )* ;
+// arguments      → expression ( "," expression )* ;
 // primary        → NUMBER | STRING | "true" | "false" | "nil"
 //                | "(" expression ")" | IDENTIFIER ;
 // program        → statement* EOF ;
-// declaration    → varDecl
+// declaration    → funDec
+//                | varDecl
 //                | statement ;
+// funDecl        → "fun" function ;
+// function       → IDENTIFIER "(" parameters? ")" block ;
+// parameters     → IDENTIFIER ( "," IDENTIFIER )* ;
 // varDecl        → "var" IDENTIFIER ( "=" expression )? ";" ;
 // statement      → exprStmt
 //                | forStmt
 //                | ifStmt
 //                | printStmt
+//                | returnStmt
 //                | whileStmt
 //                | block ;
 // exprStmt       → expression ";" ;
@@ -35,6 +41,7 @@ import static jlox.lox.TokenType.*;
 // ifStmt         → "if" "(" expression ")" statement
 //                ( "else" statement )? ;
 // printStmt      → "print" expression ";" ;
+// returnStmt     → "return" expression? ";" ;
 // whileStmt      → "while" "(" expression ")" statement ;
 // block          → "{" declaration* "}" ;
 
@@ -71,7 +78,8 @@ class Parser {
 
 	private Stmt declaration() {
 		try {
-			if (match(VAR)) return varDeclaration();
+			if (match(FUN)) return function("function");
+			else if (match(VAR)) return varDeclaration();
 			return statement();
 		} catch (ParseError error) {
 			synchronize();
@@ -83,6 +91,7 @@ class Parser {
 		if (match(FOR)) return forStatement();
 		else if (match(IF)) return ifStatement();
 		else if (match(PRINT)) return printStatement();
+		else if (match(RETURN)) return returnStatement();
 		else if (match(WHILE)) return whileStatement();
 		else if (match(LEFT_BRACE)) return new Stmt.Block(block());
 		return expressionStatement();
@@ -120,17 +129,17 @@ class Parser {
 		return body;
 	}
 
+	// ifStmt         → "if" "(" expression ")" statement
+	//                ( "else" statement )? ;
 	private Stmt ifStatement() {
 		consume(LEFT_PAREN, "Expect '(' after 'if'.");
 		Expr condition = expression();
 		consume(RIGHT_PAREN, "Expect ')' after if condition."); 
-
 		Stmt thenBranch = statement();
 		Stmt elseBranch = null;
 		if (match(ELSE)) {
 			elseBranch = statement();
 		}
-
 		return new Stmt.If(condition, thenBranch, elseBranch);
 	}
 
@@ -139,6 +148,17 @@ class Parser {
 		Expr value = expression();
 		consume(SEMICOLON, "Expect ';' after value.");
 		return new Stmt.Print(value);
+	}
+
+	// returnStmt     → "return" expression? ";" ;
+	private Stmt returnStatement() {
+		Token keyword = previous();
+		Expr value = null;
+		if (!check(SEMICOLON)) {
+			value = expression();
+		}
+		consume(SEMICOLON, "Expect ';' after return value.");
+		return new Stmt.Return(keyword, value);
 	}
 
 	// varDecl        → "var" IDENTIFIER ( "=" expression )? ";" ;
@@ -167,6 +187,26 @@ class Parser {
 		Expr expr = expression();
 		consume(SEMICOLON, "Expect ';' after expression.");
 		return new Stmt.Expression(expr);
+	}
+
+	// funDecl        → "fun" function ;
+	// function       → IDENTIFIER "(" parameters? ")" block ;
+	private Stmt.Function function(String kind) {
+		Token name = consume(IDENTIFIER, "Expect " + kind + " name.");
+		consume(LEFT_PAREN, "Expect '(' after " + kind + " name.");
+		List<Token> parameters = new ArrayList<>();
+		if (!check(RIGHT_PAREN)) {
+			do {
+				if (parameters.size() >= 255) {
+					error(peek(), "Can't have more than 255 parameters.");
+				}
+				parameters.add(consume(IDENTIFIER, "Expect parameter name."));
+			} while (match(COMMA));
+		}
+		consume(RIGHT_PAREN, "Expect ')' after parameters.");
+		consume(LEFT_BRACE, "Expect '{' before " + kind + " body.");
+		List<Stmt> body = block();
+		return new Stmt.Function(name, parameters, body);
 	}
 
 	// block          → "{" declaration* "}" ;
@@ -268,14 +308,41 @@ class Parser {
 		return expr;
 	}
 
-	// unary          → ( "!" | "-" ) unary
+	// unary          → ( "!" | "-" ) unary | call ;
 	private Expr unary() {
 		if (match(BANG, MINUS)) {
 			Token operator = previous();
 			Expr right = unary();
 			return new Expr.Unary(operator, right);
 		}
-		return primary();
+		return call();
+	}
+
+	private Expr finishCall(Expr callee) {
+		List<Expr> arguments = new ArrayList<>();
+		if (!check(RIGHT_PAREN)) {
+			do {
+				if (arguments.size() >= 255) {
+					error(peek(), "Can't have more than 255 arguments.");
+				}
+			} while (match(COMMA));
+		}
+		Token paren = consume(RIGHT_PAREN, "Expect ')' after arguments.");
+		return new Expr.Call(callee, paren, arguments);
+	}
+
+	// call           → primary ( "(" arguments? ")" )* ;
+	private Expr call() {
+		Expr expr = primary();
+
+		while (true) {
+			if (match(LEFT_PAREN)) {
+				expr = finishCall(expr);
+			} else {
+				break;
+			}
+		}
+		return expr;
 	}
 
 	// primary        → NUMBER | STRING | "true" | "false" | "nil"
